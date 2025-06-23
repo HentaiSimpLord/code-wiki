a242bb126ba36cf2ea8138110b1118296fc789f8
HentaiSimpLord <pierrefrancois1907@gmail.com>
2025-06-23 18:28:04 +0200

    update 0.0.1 (homepage)

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,11 @@
+export const Homepage = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to Code Wiki</h1>
+      <article>
+        Here I save all my Coding Progess. <br />
+        From how to create a new Webapp, to how to publish a finished Site.
+      </article>
+    </div>
+  );
+};
